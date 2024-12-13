30bd8db97e726342d4a490f0bd171b808668338b
Hitana <mcr.hitana@gmail.com>
2024-12-12 21:13:20 -0300

    Atualização do headear e pag inicio

--- js/header.js
+++ js/header.js
@@ -1,9 +1,10 @@
-// Seleciona o botão e o menu
+
+// Seleciona os elementos do menu
 const botaoMenu = document.querySelector(".botao-menu i");
 const navList = document.querySelector("#navbar ul");
 
-// Alterna a visibilidade do menu e o ícone do botão
-botaoMenu.parentElement.addEventListener("click", () => {
+// Função para alternar o menu e os ícones
+function alternarMenu() {
   navList.classList.toggle("active");
 
   // Alterna entre os ícones fa-bars e fa-times
@@ -14,4 +15,9 @@ botaoMenu.parentElement.addEventListener("click", () => {
     botaoMenu.classList.remove("fa-times");
     botaoMenu.classList.add("fa-bars");
   }
-});
+}
+
+// Adiciona o evento de clique no botão do menu
+botaoMenu.parentElement.addEventListener("click", alternarMenu);
+
+
