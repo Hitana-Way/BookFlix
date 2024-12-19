5807a37188e8abd8950230d43e1cd5c1da03671a
Hitana <mcr.hitana@gmail.com>
2024-12-18 22:43:36 -0300

    Inclusão de arquivo js para carregar pag info-livro, atualização html e css

--- js/info-livro.js
+++ js/info-livro.js
@@ -1,7 +1,67 @@
+
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const tituloLivro = urlParams.get('titulo');
+
+
+fetch('./json/info-livro.json')  // Caminho para o arquivo JSON  
+    .then(response => response.json())  // Converte a resposta para um JSON
+    .then(livros => {
+
+        let livroEncontrado = null;  // iniciando como null
+        for (let i = 0; i < livros.length; i++) {  // Percorrendo os livros no JSON
+            if (livros[i].titulo === tituloLivro) {  // comparar se é igual ao tituloLivros
+                livroEncontrado = livros[i];  // add o livro encontrado
+                break;   
+            }
+        }
+
+        if (livroEncontrado) {  // se o livro foi encontrado
+            const livroInfo = `
+                <div class="livro">
+                    <img src="${livroEncontrado.imagem}" width="300" height="400" alt="Capa ${livroEncontrado.titulo}" class="capa">
+                    <div class="informacoes">
+                        <button class="favoritar">
+                            <i class="fa-regular fa-heart"></i>
+                        </button>
+                        <h2>${livroEncontrado.titulo}</h2>
+                        <p><strong>Edição Português</strong> | por ${livroEncontrado.autor} (Autor), ${livroEncontrado.tradutor} (Tradutor). <br></p>
+                        <p>${livroEncontrado.descricao}</p>
+                        <br>
+                        <section class="acoes">
+                            <button>Leia online <i class="fas fa-book"></i></button>
+                            <button>Audiobook <i class="fas fa-headphones"></i></button>
+                            <button>Baixar <i class="fas fa-download"></i></button>
+                            <button>Comentar <i class="fas fa-comment"></i></button>
+                        </section>
+                        <div class="avaliacao">
+                            <div class="estrelas">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p class="detalhes">Avaliações: ${livroEncontrado.avaliacoes} | Comentários: ${livroEncontrado.comentarios}</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+            
+            // Adiciona as informações na página
+            document.getElementById('livro-info').innerHTML = livroInfo;
+        }
+    });
+
+
+// -------------------------------BOTÃO FAVORITAR -------------------- //
+//BOTÃO AINDA ESTÁ EM TESTE //
+ 
 // Captura o botão de favoritar com o icone de coração
 let coracao = document.querySelector(".favoritar i");
 
-// Função
+// Função para favoritar o livro
 function favoritar (){ 
     if (coracao.classList.contains("fa-regular", "fa-heart")) {
         coracao.classList.remove("fa-regular", "fa-heart");        
@@ -12,5 +72,5 @@ function favoritar (){
     }
 }
 
-// Evento
+// Evento de clique no ícone de favoritar
 coracao.addEventListener("click", favoritar);
